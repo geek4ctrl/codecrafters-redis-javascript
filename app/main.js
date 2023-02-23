9ad59a236b86bd27df2f18f8bced89f2159eff7b
geek4ctrl <laurent.mb19@gmail.com>
2023-02-23 11:19:15 +0200

    Handle concurrent clients

--- app/main.js
+++ app/main.js
@@ -6,8 +6,10 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
+
         console.log('Data: ', data);
         connection.write("+PONG\r\n");
+
     })
 });
 
